f2831dc2e2d8b3783f6daedcc8fdffca9d6dea52
HimanshiBorad <109951373+HimanshiBorad@users.noreply.github.com>
2022-07-25 14:23:31 +0530

    Add files via upload

---
+++ MaxofThree.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class MaxofThree
+{
+	public static void main(String[] args)
+	{
+		int a, b, c, max;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter three numbers:");
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c = sc.nextInt();
+		max = (a > b) ? ((a > c)?a:c) : ((b > c)?b:c);
+		System.out.println("Maximum of three numbers is " +max);
+	}
+}
